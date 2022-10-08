73f015d2feb7eea1a987d6eb2b66050e6d7f5010
Captured-Heart <you@example.com>
2022-10-08 13:18:13 +0100

    File Structures created

---
+++ lib/util/utils/brand_colors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class BrandColors {
+  static const Color colorPrimary = Color(0xFF2B1A64);
+  static const Color colorPrimaryDark = Color(0xFF1c3aa9);
+  static const Color colorAccent = Color(0xFF21ba45);
+  static const Color colorAccent1 = Color(0xFF7AB432);
+
+  static const Color colorBackground = Color(0xFFFBFAFF);
+
+  static const Color colorPink = Color(0xFFE66C75);
+  static const Color colorOrange = Color(0xFFE8913A);
+  static const Color colorBlue = Color(0xFF2254A3);
+  static const Color colorAccentPurple = Color(0xFFf4f2fa);
+  static const Color colorBrown = Color(0xFF383635);
+  static const Color colorText = Color(0xFF383635);
+  static const Color colorTextLight = Color(0xFF918D8D);
+  static const Color colorTextDark = Color(0xFF292828);
+
+  static const Color colorGreen = Color(0xFF21ba45);
+  static const Color colorLightGray = Color(0xFFe2e2e2);
+  static const Color colorDimText = Color(0xFFadadad);
+}
